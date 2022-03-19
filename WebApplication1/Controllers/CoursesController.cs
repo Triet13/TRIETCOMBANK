fd0385563c80bc0a582ba70ea44714fdaff22560
triet <lyhuynhminhtriet1@gmail.com>
2022-03-19 19:38:15 +0700

    first commit

---
+++ WebApplication1/Controllers/CoursesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+using Microsoft.AspNet.Identity;
+
+namespace WebApplication1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = (IEnumerable<Microsoft.Build.Framework.XamlTypes.Category>)(IEnumerable<Category>)_dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/CourseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework.XamlTypes;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.ViewModels
+{
+    public class CourseViewModel
+    {
+        public  string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+       
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
+//ok eroi do may nhu cc luc ra nay luc ra kia 
